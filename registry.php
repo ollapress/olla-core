beac81cf0afe94aa8722a244d93bf128aec6fa97
ollatech <olla.code@gmail.com>
2018-03-26 02:05:31 +0700

    initial

---
+++ registry.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Olla
+ */
+/**
+ * Plugin Name: Olla
+ * Plugin URI: http://www.olla.io
+ * Description: Olla core system
+ * Version: 1.0.0
+ * Author: Olla Digital
+ * Author URI: http://www.olla.io
+ * Text Domain: olla
+ * Domain Path: /languages/
+ * License: GPL2
+ */
+
+define( 'OLLA_VERSION', '1.0.0' );
